85749a9c0e3331c2a7b7c90131f4168d5613f6c5
岩田康平 <iwater0715@gmail.com>
2021-02-06 10:30:25 +0900

    執筆

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: `K.Iwata`,
       summary: `K.Iwata's Gatsby BLOG`,
     },
-    description: `This is K.Iwata BLOG.`,
+    description: `こちらはフロントエンドエンジニアK.Iwataのブログサイトになります。日々の業務の中や個人制作等で学んだTipsをまとめ、記事として残していきます。`,
     siteUrl: `https://k-blog.life`,
     social: {
       twitter: `webD_hello21`,
